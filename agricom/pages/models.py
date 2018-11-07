344a25cceeff947bfda2c020ee0ebefd94c81c38
agnesimbs <agneswambui504@gmail.com>
2018-11-07 16:19:13 +0300

    ..

---
+++ agricom/pages/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+# Create your models here.
+class Movers(models.Model):
+	name=models.CharField(max_length=20)
+	location=models.CharField(max_length=20)
+	services=models.CharField(max_length=20)
+
+	def _unicode_(self):
+		return self.name
+	class Meta:
+		verbose_name_plural ="Movers"
+
+class Bookings(models.Model):
+	name=models.CharField(max_length=20)
+	location=models.CharField(max_length=20)
+
+	def _unicode_(self):
+		return self.name
+	class Meta:
+		verbose_name_plural ="Bookings"
